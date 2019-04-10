d200191387d242e2c59a219ca046244418983f24
Kelly Cook <kellycook20878@gmail.com>
2019-04-10 09:56:44 -0500

    added dependencies to gemfile

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+    # the index controller will control the page that will display all of the posts from all of the users.
+    # think of it as the Reddit homepage or something
+    def index
+    end
+end
