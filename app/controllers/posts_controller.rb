2e4cfcb38af3901b55d58def1fb46558cc126a54
Kelly Cook <kellycook20878@gmail.com>
2019-04-10 10:46:43 -0500

    set up a pay to create a new post in the postscontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,30 @@ class PostsController < ApplicationController
     # think of it as the Reddit homepage or something
     def index
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+
+    # requiring a title and content of the post
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
