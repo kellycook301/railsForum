e0f0d6dc62b31ca3026de7faa3bcdcc718c52fed
Kelly Cook <kellycook20878@gmail.com>
2019-04-10 14:00:48 -0500

    delete and edit work / route to index page as well

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
     # the index controller will control the page that will display all of the posts from all of the users.
     # think of it as the Reddit homepage or something
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+
     def index
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
@@ -19,10 +22,29 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @post.destroy
+        redirect_to root_path
     end
 
     private
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
 
     # requiring a title and content of the post
     def post_params
